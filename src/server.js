737d771bc903c79e5635496cfe00ecce5e763f0c
Farid-cr <faridgarnica4@gmail.com>
2026-06-04 22:10:10 -0300

    feat: se añadio ruta raiz Closes #2

--- src/server.js
+++ src/server.js
@@ -15,5 +15,8 @@ const usuarios = [
 ];
 
 // TODO: Define tus rutas aquí
+app.get('/', (req, res) => {
+  res.status(200).send('Bienvenid@s a nuestro servidor Express!');
+});
 
 module.exports = app;
